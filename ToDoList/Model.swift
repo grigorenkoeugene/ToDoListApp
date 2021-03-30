159ba30e954fa0ee9636958a14e9013a6e7c7121
Jenya <7327917@gmail.com>
2021-03-30 15:14:39 +0300

    ToDo

---
+++ ToDoList/Model.swift
@@ -0,0 +1,45 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by Admin on 12/04/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+
+var ToDoItems: [[String: Any]] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get {
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]]{
+            return array
+        } else {
+            return []
+        }
+    }
+}
+
+func addItem(nameItem: String, isCompleted: Bool = false ) {
+    ToDoItems.append(["Name": nameItem, "isCompleted": isCompleted])
+}
+
+func removeItem(at index: Int) {
+    ToDoItems.remove(at: index)
+}
+
+func moveItem(fromIndex: Int, toIndex: Int){
+    let from = ToDoItems[fromIndex]
+    ToDoItems.remove(at: fromIndex)
+    ToDoItems.insert(from, at: toIndex)
+}
+
+func changeState(at item: Int) -> Bool{
+    ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
+
+    return ToDoItems[item]["isCompleted"] as! Bool
+}
+
+
